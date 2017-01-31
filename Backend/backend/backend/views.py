dba51d28c3cf3eed346f6f4acd3a06b4be32f9d4
Mihai Mandrescu <michael@michaelsmbp.home>
2017-01-31 22:12:47 +0000

    Fixed url

--- Backend/backend/backend/views.py
+++ Backend/backend/backend/views.py
@@ -1,13 +1,14 @@
 import json
 
 from django.http import JsonResponse
+from django.http import Http401
 from django.views.decorators.http import require_http_methods
 
 
-@require_http_methods(["POST"])
+@require_http_methods(["GET"])
 def login(request):
     request_headers = request.META
     request_json = json.loads(request.body)
     print "Headers: %s" % request_headers
     print "Body: %s" % request_json
-    return JsonResponse({"auth": False})
+    raise Http401("Bad token")
