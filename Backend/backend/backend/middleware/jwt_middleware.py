542e745fd222102198ed1ebfd838429515ba760f
Mihai Mandrescu <michael@michaelsmbp.home>
2017-01-31 22:54:23 +0000

    Custom middleware

--- Backend/backend/backend/middleware/jwt_middleware.py
+++ Backend/backend/backend/middleware/jwt_middleware.py
@@ -1,13 +1,20 @@
 import jwt
+import base64
 
 def jwt_middleware(get_response):
     def middleware(request):
+        print "All headers: %s" % request.META
+        auth_header = request.META.get("Authorization")
+        print "Auth: %s" % base64.b64decode(auth_header)
         encoded_token = request.META.get('HTTP_X_AUTH_TOKEN')
-        decoded_token = jwt.decode(
-            encoded_token,
-            'secret',
-            algorithms=['HS256']
-        )
+        if encoded_token:
+            decoded_token = jwt.decode(
+                encoded_token,
+                auth_header,
+                algorithms=['HS256']
+            )
+        else:
+            print "No auth_header."
 
         print "MIDDLEWARE: Decoded token: %s" % decoded_token
 
