29f5ee3da7adfcbef37be36649d1cdd0361276e9
Mihai Mandrescu <michael@michaelsmbp.home>
2017-01-31 22:45:47 +0000

    Custom middleware

--- Backend/backend/backend/middleware/jwt_middleware.py
+++ Backend/backend/backend/middleware/jwt_middleware.py
@@ -2,7 +2,7 @@ import jwt
 
 def jwt_middleware(get_response):
     def middleware(request):
-        encoded_token = request.META.get('REMOTE_ADDR').
+        encoded_token = request.META.get('HTTP_X_AUTH_TOKEN')
         decoded_token = jwt.decode(
             encoded_token,
             'secret',
