6811f6e67eb64dddf3f5a0d60a575d5b1113b19f
michael <mihai@mandrescu.co>
2017-02-16 23:51:15 +0000

    Static file serving

--- Backend/backend/backend/views.py
+++ Backend/backend/backend/views.py
@@ -1,20 +1,21 @@
 from . Utils.MongoRouter import MongoRouter
 
-from django.http import JsonResponse, StreamingHttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.core.files.storage import get_storage_class
 
 from . import settings
 
-INDEX_HTML = '/index.html'
+INDEX_HTML = 'index.html'
 
 router = MongoRouter()
 
 
 def main(request):
     storage_class = get_storage_class(settings.STATICFILES_STORAGE)
-    index_file = storage_class().open(INDEX_HTML)
-    return StreamingHttpResponse(index_file)
+    with storage_class().open(INDEX_HTML) as index_file:
+        html_content = index_file.read()
+    return HttpResponse(html_content)
 
 
 @require_http_methods(["GET", "OPTIONS"])
