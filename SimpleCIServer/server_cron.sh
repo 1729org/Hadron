ac496a98dbbc9e3b7bbb3d21bdaa299265ece2ee
michael <mihai@mandrescu.co>
2017-02-18 18:03:38 +0000

    Server restart cron

--- SimpleCIServer/server_cron.sh
+++ SimpleCIServer/server_cron.sh
@@ -2,7 +2,9 @@
 
 
 $SCI_RESULT=$(ps aux | grep "/usr/bin/python SimpleCIServer/simple_ci_server.py" | wc -l)
-if [ "$SCI_RESULT" == 1 ]; then
+echo "SCI_RESULT: $SCI_RESULT"
+
+if [ "$SCI_RESULT" -eq 1 ]; then
     echo "Simple CI server has died. RIP. Now restarting."
     CURRENT_DIR=$(pwd)
     cd /home/ubuntu/Hardon
@@ -12,8 +14,10 @@ fi
 
 
 $BKD_RESULT=$(ps aux | grep "/home/ubuntu/Hadron/Backend/backend_env/bin/python Backend/backend/manage.py runserver 0.0.0.0:8000" | wc -l)
-if [ "$BKD_RESULT" == 1 ]; then
-    echo "Simple CI server has died. RIP. Now restarting."
+echo "BKD_RESULT: $BKD_RESULT"
+
+if [ "$BKD_RESULT" -eq 1 ]; then
+    echo "Backend server has died. RIP. Now restarting."
     CURRENT_DIR=$(pwd)
     cd /home/ubuntu/Hardon
     Backend/backend_env/bin/python /home/ubuntu/Hadron/Backend/backend/manage.py runserver 0.0.0.0:8000
